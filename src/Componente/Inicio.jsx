0322a898b7cbe6b5366f7a953a5c2021f1d0458e
Alex <gustavo.herrera03@epn.edu.ec>
2025-06-11 21:31:36 -0500

    Estilo MiAutoCheck

--- src/Componente/Inicio.jsx
+++ src/Componente/Inicio.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import LogoAzul from '../Imagenes/LogoAzul.svg';
-import { Link } from "react-router-dom";
+
 
 
 function Inicio({usuario}) {
@@ -10,7 +10,7 @@ function Inicio({usuario}) {
         <div>
             <img src={LogoAzul} alt="Logo AutoCheck" style={{ width: '200px' }} />
             <h1>Bienvenido al inicio</h1>
-            <Link to="/MiAutoCheck" className="btnInicio">Ir a Mi Auto Check</Link>
+            
         </div>
     );
 }
